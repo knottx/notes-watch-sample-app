cbe18a0a756a596d3ad9313eb39ae3c6e83163b4
Knot <knotto.vt@gmail.com>
2022-04-03 16:38:37 +0700

    add files

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Visarut Tippun on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
